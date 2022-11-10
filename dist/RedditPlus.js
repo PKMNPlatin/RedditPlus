e6a1c3bb0957b8b3a9963912505edca5ec5d566b
PKMNPlatin <jschmidt@dh-software.de>
2022-11-10 11:07:34 +0100

    Inital commit

---
+++ dist/RedditPlus.js
@@ -0,0 +1,11 @@
+var Test = (function () {
+    function Test() {
+    }
+    Test.prototype.hello = function () {
+        console.log("Hello World");
+    };
+    return Test;
+}());
+var test = new Test();
+test.hello();
+//# sourceMappingURL=RedditPlus.js.map
